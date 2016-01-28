768dc4962e637b489085494d5ea23e2128689fdc
Dylan Lott <lott.dylan@gmail.com>
2016-01-28 11:28:12 -0700

    working on shit

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def home(request):
-	return render(request, "main/base.html")
+	return render(request, "main/dashboard.html")
--- worlds/views.py
+++ worlds/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def addWorld(request):
+	if request.method == 'POST': 
+		form = WorldForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/')
+	else: 
+		form = WorldForm()
+
+	return render_to_response('addWorld.html', {
+		'form': form,
+		})
