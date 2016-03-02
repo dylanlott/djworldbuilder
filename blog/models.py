b4b6ff41b407c26f80f90e9e65834030238d3ab3
Dylan Lott <lott.dylan@gmail.com>
2016-03-01 21:43:33 -0700

    added the created_by field to the post

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model): 
     title = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
+    updated_by=models.ForeignKey(User, related_name='updated_by_user')
+    created_by=models.ForeignKey(User, related_name='created_by_user')
 
     def __str__(self):
         return "{0}".format(self.name)
