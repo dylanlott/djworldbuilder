69c47659530ad209162b7d8ca6b01dce096f7127
Dylan Lott <lott.dylan@gmail.com>
2016-03-15 23:09:11 -0600

    working on adding the current user to every model

--- worlds/models.py
+++ worlds/models.py
@@ -5,25 +5,28 @@ from django.forms import ModelForm
 # Create your models here.
 
 class Universe(models.Model): 
-	name = models.CharField(max_length=100)
-	date_created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='universe_created_by')
 
-	def __str__(self):
-		return "{0}".format(self.name)
+    def __str__(self):
+        return "{0}".format(self.name)
 
 class SolarSystem(models.Model):
-	name = models.CharField(max_length=100)
-	date_created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='solarsystem_created_by')
 
-	def __str__(self): 
-		return "{0}".format(self.name)
+    def __str__(self): 
+        return "{0}".format(self.name)
 
 class World(models.Model): 
-	name = models.CharField(max_length=60)
-	date_created = models.DateTimeField(auto_now_add=True)
-	climate = models.CharField(max_length=100)
-	solar_system = models.ForeignKey(SolarSystem)
-	universe = models.ForeignKey(Universe)
+    name = models.CharField(max_length=60)
+    date_created = models.DateTimeField(auto_now_add=True)
+    climate = models.CharField(max_length=100)
+    solar_system = models.ForeignKey(SolarSystem)
+    universe = models.ForeignKey(Universe)
+    created_by = models.ForeignKey(User, related_name='world_created_by')
 
-	def __str__(self): 
-		return "{0}".format(self.name)
+    def __str__(self): 
+        return "{0}".format(self.name)
--- worlds/views.py
+++ worlds/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
+from .models import World
+from .forms import WorldForm
 
 # Create your views here.
 
 def view_worlds(request):
-    return render(request, 'worlds/worlds.html', {})
+    ctx = {}
+
+    current_user = request.user
+    all_worlds = World.objects.all().filter(created_by = current_user)
+
+    return render(request, 'worlds/worlds.html', {all_worlds: all_worlds})
 
 def addWorld(request):
-	if request.method == 'POST': 
-		form = WorldForm(request.POST)
-		if form.is_valid():
-			return HttpResponseRedirect('/')
-	else: 
-		form = WorldForm()
+    if request.method == 'POST': 
+        form = WorldForm(request.POST)
 
-	return render_to_response('addWorld.html', {
-		'form': form,
-		})
+    if form.is_valid():
+        return HttpResponseRedirect('/')
+    else: 
+        form = WorldForm()
+
+    return render_to_response('addWorld.html', {
+    'form': form,
+    })
