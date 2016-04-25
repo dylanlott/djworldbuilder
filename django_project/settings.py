d3329676ed31f15f9e8f3e59b457bbc135196672
Dylan Lott <lott.dylan@gmail.com>
2016-04-24 21:04:44 -0600

    fixing static variable

--- django_project/settings.py
+++ django_project/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(PACKAGE_ROOT, 'static'),)
 
 LOGIN_URL = 'worldbuilder_login' 
 LOGOUT_URL = 'worldbuilder_logout' 
